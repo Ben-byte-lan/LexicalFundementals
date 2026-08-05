ef3217851642030d2127ff4c9049093b6e31bb32
Ben-byte-lan <benjaminhallen5@gmail.com>
2026-08-05 16:21:13 -0400

    Added Ngram based functions

--- Features/Ngram.py
+++ Features/Ngram.py
@@ -1,4 +1,21 @@
-try:
-    from LexicalFundementals.Features.Tools import FetchNgrams
-except ImportError:
-    from Features.Tools import FetchNgrams
+from .Tools import FetchNgrams
+from collections import Counter
+from typing import Sequence
+from .Lexical import Shannon_Entropy, TTR
+
+
+
+
+def NgramFreq(tokens: Sequence[str], n:int)->dict:
+    Ngrams =FetchNgrams(tokens, n)
+    NgramsC = Counter(FetchNgrams(tokens, n))
+    return dict(map(lambda item: (item[0], item[1]/len(Ngrams)), NgramsC.items()))
+    
+def TopNgrams(tokens:Sequence[str], n:int,k:int)->dict:
+    return Counter(FetchNgrams(tokens, n)).most_common(k)
+
+def NgramEntropy(tokens:Sequence[str], n:int)->dict:
+    return Shannon_Entropy(FetchNgrams(tokens,n))
+
+def NgramTTR(tokens, n):
+    return TTR(FetchNgrams(tokens,n))
